2877f735ca35b5ee4de1076e085ae110a9a4b219
mitzisong <mitzisong@gmail.com>
2014-02-11 13:08:54 -0800

    Creates dictionary of restaurants and scores, and returns it alphabetically sorted.

---
+++ sorted_data.py
@@ -0,0 +1,49 @@
+
+
+# Output should look like this:
+    # Restaurant 'Andalu' is rated at 3.
+    # Restaurant "Arinell's" is rated at 4.
+    # Restaurant 'Bay Blend Coffee and Tea' is rated at 3.
+    # Restaurant 'Casa Thai' is rated at 2.
+    # Restaurant 'Charanga' is rated at 3.
+    # Restaurant 'El Toro' is rated at 5.
+    # Restaurant 'Giordano Bros' is rated at 2.
+    # Restaurant "Irma's Pampanga" is rated at 5.
+    # Restaurant 'Little Baobab' is rated at 1.
+    # Restaurant 'Pancho Villa' is rated at 3.
+    # Restaurant 'Taqueria Cancun' is rated at 2.
+    # Restaurant 'Urbun Burger' is rated at 1.
+
+# Read file
+f = open('scores.txt')
+
+# Initiate empty dict
+rest_scores = {}
+
+# TODO Why does this print '/n' behind each 2nd tuple? Can use this in place of readline()? (try while lloop)
+# Use readline to go through each line to split key/value pairs by colon(:)
+for line in f:
+    line = line.strip()
+    split_line = line.split(":")
+    rest_scores[split_line[0]] = split_line[1]
+# print "Restaurant %s is rated at %s" % (, rest_scores[keys])
+
+# Return ratings in alphabetical order by restaurant
+alpha_rest = sorted(rest_scores.keys())
+for restaurant in alpha_rest:
+    print "Restaurant %s is rated at %s" % (restaurant, rest_scores[restaurant])
+
+
+
+
+
+
+
+"""
+How to use readline: 
+line = f.readline()
+while line is not None:
+    line = line.strip()
+
+    line = f.readline()
+"""
